61bf007397c6ba31939d78f80efd1946ed01bdad
Phil Weir <phil.weir@flaxandteal.co.uk>
2025-01-02 16:19:24 +0000

    feat(mod_web_browser): add PK4 content and remove lantern references

---
+++ examples/therustymod_web/addon/mod_web_browser/README.md
@@ -0,0 +1,6 @@
+# mod_web_browser
+
+ZIP this as a PK4 and put it in the home directory
+beside `libtherustymod_web.so` built from the directory
+above, and you should get a web browser on `localhost:9797`
+that will start showing "init" when you load a game.
--- examples/therustymod_web/src/lib.rs
+++ examples/therustymod_web/src/lib.rs
@@ -7,7 +7,7 @@ mod log;
 use therustymod_gen::{therustymod_lib};
 
 #[therustymod_lib(daemon=true)]
-mod mod_forward_lantern {
+mod mod_web_browser {
     use crate::http::launch;
 
     async fn __run() {
@@ -15,11 +15,11 @@ mod mod_forward_lantern {
         launch().await
     }
 
-    fn init_mod_forward_lantern() {
+    fn init_web_browser() {
         log::add_to_log("init".to_string())
     }
 
-    fn do_stuff_mod_forward_lantern() {
-        log::add_to_log("do stuff".to_string())
+    fn do_log_to_web_browser() {
+        log::add_to_log("doing stuff".to_string())
     }
 }
